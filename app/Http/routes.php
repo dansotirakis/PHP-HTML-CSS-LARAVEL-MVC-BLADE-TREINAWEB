f837fb6811f0db647d6958cec1e2f1428f1a6986
Damianos Sotirakis <dansotirakis18@gmail.com>
2018-03-25 18:58:54 -0300

    Retomada 4 - 3

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,6 +10,10 @@
 | and give it the controller to call when that URI is requested.
 |
 */
+Route::get('/test',function(){
+    return 'TreinaWeb';
+});
+
 
 Route::get('/', function () {
     return view('welcome');
